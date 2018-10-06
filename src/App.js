99ffe100fe9d769bff04ff344c47bfe9ac89160f
Pete Vilter <vilterp@cockroachlabs.com>
2018-10-05 21:54:55 -0400

    page to paste things in

--- src/App.js
+++ src/App.js
@@ -2,9 +2,83 @@ import React from "react";
 import TraceAndSidebar from "./TraceAndSidebar";
 import exampleTrace from "./exampleTrace";
 
+function parseTrace(text) {
+  return JSON.parse(text);
+}
+
+function serializeTrace(trace) {
+  return JSON.stringify(trace, null, 2);
+}
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      parseError: null,
+      traceText: "",
+      trace: null,
+    };
+  }
+
+  handleChangeTraceText = (evt) => {
+    this.setState({
+      traceText: evt.target.value,
+    });
+  }
+
+  handleSubmit = () => {
+    try {
+      const trace = parseTrace(this.state.traceText);
+      this.setState({
+        trace,
+      });
+    } catch (e) {
+      this.setState({
+        parseError: e,
+      });
+    }
+  }
+
+  handleExample = () => {
+    this.setState({
+      trace: exampleTrace,
+    });
+  }
+
+  handleClearTrace = () => {
+    this.setState({
+      trace: null,
+      traceText: serializeTrace(this.state.trace),
+    });
+  }
+
   render() {
-    return <TraceAndSidebar trace={exampleTrace} />
+    if (this.state.trace === null) {
+      return (
+        <div>
+          <textarea
+            value={this.state.traceText}
+            onChange={this.handleChangeTraceText}
+            style={{ fontFamily: "monospace" }}
+            cols="80"
+            rows="30"
+          />
+          <br />
+          <button onClick={this.handleSubmit}>Visualize</button>
+          <br />
+          {this.state.parseError ? <pre>Parse error: {this.state.parseError}</pre> : null}
+          <br />
+          <button onClick={this.handleExample}>Example</button>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <button onClick={this.handleClearTrace}>Clear Trace</button>
+        <TraceAndSidebar trace={this.state.trace} />
+      </div>
+    );
   }
 }
 
