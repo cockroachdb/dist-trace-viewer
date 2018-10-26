95d7d74c8b07b3486660664e319d6505470fd5a1
Bram Gruneir <bram@cockroachlabs.com>
2018-10-26 16:38:44 -0400

    tree parsing

--- src/csvToTrace.js
+++ src/csvToTrace.js
@@ -1,9 +1,11 @@
 import _ from "lodash";
 import Papa from "papaparse";
+import DateTime from 'luxon/src/datetime.js'
+import Duration from "luxon/src/duration";
 
 export function parseTrace(csvText) {
-  const parsedCSV = parseCSV(csvText);
-  return rowsToTree(parsedCSV)
+    const parsedCSV = parseCSV(csvText);
+    return rowsToTree(parsedCSV)
 }
 
 const SPAN_START = "=== SPAN START: ";
@@ -14,68 +16,89 @@ const AGE_COL = 8;
 const MESSAGE_COL = 7;
 
 function rowsToTree(rows) {
-  function recur(startIdx) {
-    const startRow = rows[startIdx];
-    const startRowMessage = startRow[MESSAGE_COL];
-    const spanIdx = startRow[SPAN_IDX_COL];
-    const logMessages = [];
-    const children = [];
-    let i = startIdx + 1;
-    while (i < rows.length) {
-      const row = rows[i];
-      const rowMessage = row[MESSAGE_COL];
-      if (row[SPAN_IDX_COL] === spanIdx) {
-        logMessages.push({
-          age: row[AGE_COL],
-          message: rowMessage,
-        });
-      } else if (rowMessage.startsWith(SPAN_START)) {
-        const { node, endIdx } = recur(i);
-        children.push(node);
-        i = endIdx;
-      }
-      i++;
-    }
+    const tree = rows[0];
+    let stack = [rows[0]];
+    rows.slice(1).forEach(row => {
+        let cur = _.last(stack);
+        if (row.spanID > cur.spanID) {
+            cur.children.push(row);
+            stack.push(row);
+        } else {
+            while (cur.spanID !== row.spanID) {
+                stack.pop()
+                cur = _.last(stack);
+            }
+            cur.messages.push(...row.messages);
+        }
+    });
+    return tree;
+}
 
+
+
+const durationRegex = /(?:(\d*)h)?(?:(\d*)m)?(?:(\d*)s)?(?:(\d*)ms)?(?:(\d*)μs)?(?:(\d*)ns)?/
+
+// returns nanoseconds
+function parseDuration(dur) {
+    const matches = dur.match(durationRegex);
+    const hours = matches[1] ? parseInt(matches[1]) : 0;
+    const minutes = 60 * hours + (matches[2] ? parseInt(matches[2]) : 0);
+    const seconds = 60 * minutes + (matches[3] ? parseInt(matches[3]) : 0);
+    const milliseconds = 1000 * seconds + (matches[4] ? parseInt(matches[4]) : 0);
+    const microseconds = 1000 * milliseconds + (matches[5] ? parseInt(matches[5]) : 0);
+    const nanoseconds = 1000 * microseconds + (matches[6] ? parseInt(matches[6]) : 0);
+    return nanoseconds;
+}
+
+function parseRow(columns) {
+    if (columns.length !== EXPECTED_HEADING.length) {}
     return {
-      node: {
-        id: rows[startIdx],
-        name: startRowMessage.slice(SPAN_START.length, startRowMessage.length - SPAN_END.length),
-        startTS: 0,
-        duration: 10,
-        props: {},
-        children,
-        logMessages,
-      },
-      endIdx: i,
-    };
-  }
+        spanID: parseInt(columns[0]),
+        timestamp: DateTime.fromSQL(columns[2], { zone: 'utc' }),
+        duration: parseDuration(columns[3]),
+        operation: columns[4],
+        location: columns[5],
+        tag: columns[6],
+        messages: [{
+            id: parseInt(columns[1]),
+            message: columns[7],
+            age: parseDuration(columns[8]),
+        }],
+        children: [],
+    }
+}
 
-  const message = rows[1][MESSAGE_COL];
-  if (!message.startsWith(SPAN_START)) {
-    throw new Error(`expected SPAN START; found ${message}`);
-  }
-
-  return recur(1).node; // skip header
+function compressRows(originalRows) {
+    let updatedRows = [originalRows[0]]
+    originalRows.slice(1).forEach(row => {
+        const last = _.last(updatedRows)
+        if (row.spanID === last.spanID) {
+            last.messages.push(...row.messages);
+        } else {
+            updatedRows.push(row);
+        }
+    })
+    return updatedRows;
 }
 
 function parseCSV(csvText) {
-  const parseRes = Papa.parse(csvText);
+    const parseRes = Papa.parse(csvText);
+    if (parseRes.errors.length > 0) {
+        throw new Error(`parse errors: ${parseRes.errors.join(", ")}`);
+    }
+    if (parseRes.data.length === 0) {
+        throw new Error(`parse error: 0 rows`);
+    }
+    let rows = parseRes.data;
+    const header = rows[0];
+    if (!_.isEqual(header, EXPECTED_HEADING)) {
+        throw new Error(`expected first row ${EXPECTED_HEADING.join(',')}; got ${header.join(',')}`);
+    }
 
-  if (parseRes.errors.length > 0) {
-    throw new Error(`parse errors: ${parseRes.errors.join(", ")}`);
-  }
+    const parsedRows = _.map(rows.slice(1), row => parseRow(row));
+    const compRows = compressRows(parsedRows);
+    const tree = rowsToTree(compRows);
+    debugger
 
-  if (parseRes.data.length === 0) {
-    throw new Error(`parse error: 0 rows`);
-  }
-
-  const rows = parseRes.data;
-  const header = rows[0];
-
-  if (!_.isEqual(header, EXPECTED_HEADING)) {
-    throw new Error(`expected first row ${EXPECTED_HEADING.join(',')}; got ${header.join(',')}`);
-  }
-
-  return rows;
+    return tree;
 }
